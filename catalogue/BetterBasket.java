c142da2d6d5642d45b6a1181d3845d9488563b49
Cooba North <coobanorth@gmail.com>
2024-11-26 18:02:01 +0000

    BetterBasket
    started better basket, the quantities add but don't currently reorder from high to low

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -9,12 +9,19 @@ import java.util.Collections;
  * @author  Your Name 
  * @version 1.0
  */
-public class BetterBasket extends Basket implements Serializable
-{
-  private static final long serialVersionUID = 1L;
-  
-
-  // You need to add code here
-  // merge the items for same product,
-  // or sort the item based on the product number
+public class BetterBasket extends Basket{
+  @Override
+  public boolean add( Product pr)
+  {
+    for(Product prInList: this){
+      if(prInList.getProductNum().equals(pr.getProductNum())) {
+        int quantity = pr.getQuantity()+prInList.getQuantity();
+        prInList.setQuantity(quantity);
+        return (true);
+      }
+    }
+    super.add( pr );     // Call add in ArrayList
+    Collections.sort(this);
+    return (true);
+  }
 }
