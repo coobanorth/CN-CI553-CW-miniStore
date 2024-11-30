c96e7b309b628a7e5f68ddda0d859fcdf801147c
Cooba North <coobanorth@gmail.com>
2024-11-30 17:57:07 +0000

    CheckByName complete

--- clients/customer/CustomerView.java
+++ clients/customer/CustomerView.java
@@ -20,6 +20,7 @@ public class CustomerView implements Observer
   class Name                              // Names of buttons
   {
     public static final String CHECK  = "Check";
+    public static final String CHECKName  = "Search";
     public static final String CLEAR  = "Clear";
   }
 
@@ -32,6 +33,7 @@ public class CustomerView implements Observer
   private final JTextArea   theOutput  = new JTextArea();
   private final JScrollPane theSP      = new JScrollPane();
   private final JButton     theBtCheck = new JButton( Name.CHECK );
+  private final JButton     theBtCheckName = new JButton( Name.CHECKName );
   private final JButton     theBtClear = new JButton( Name.CLEAR );
 
   private Picture thePicture = new Picture(80,80);
@@ -72,7 +74,14 @@ public class CustomerView implements Observer
       e -> cont.doCheck( theInput.getText() ) );
     cp.add( theBtCheck );                           //  Add to canvas
 
-    theBtClear.setBounds( 16, 25+60*1, 80, 40 );    // Clear button
+    theBtCheckName.setBounds( 16, 25+60*1, 80, 40 );    // Check button
+    theBtCheckName.setBackground(Color.CYAN);
+    theBtCheckName.setForeground( Color.BLACK );
+    theBtCheckName.addActionListener(                   // Call back code
+            e -> cont.doCheckByName( theInput.getText() ) );
+    cp.add( theBtCheckName );                           //  Add to canvas
+
+    theBtClear.setBounds( 16, 25+60*2, 80, 40 );    // Clear button
     theBtClear.addActionListener(                   // Call back code
       e -> cont.doClear() );
     cp.add( theBtClear );                           //  Add to canvas
@@ -91,7 +100,7 @@ public class CustomerView implements Observer
     cp.add( theSP );                                //  Add to canvas
     theSP.getViewport().add( theOutput );           //  In TextArea
 
-    thePicture.setBounds( 16, 25+60*2, 80, 80 );   // Picture area
+    thePicture.setBounds( 16, 25+60*3, 80, 80 );   // Picture area
     cp.add( thePicture );                           //  Add to canvas
     thePicture.clear();
     
